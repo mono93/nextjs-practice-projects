a609012e6575506a8b50ae53044697e0e0c34415
Monojit Saha <monojitsaha@Monojits-MacBook-Pro.local>
2025-03-22 12:04:09 +0530

    optimization options

--- nextjs-pratice-project-1/pages/index.js
+++ nextjs-pratice-project-1/pages/index.js
@@ -1,9 +1,14 @@
 import { getFeaturedEvents } from "../helpers/api-utils";
 import EventList from "../components/events/event-list";
+import Head from "next/head";
 
 const HomePage = (props) => {
   return (
     <div>
+      <Head>
+        <title> Next.js Events </title>
+        <meta name="description" content="A list of awesome events" />
+      </Head>
       <EventList items={props.events} />
     </div>
   );
