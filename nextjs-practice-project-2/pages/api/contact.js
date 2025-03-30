f7fe6aaf194e43c4064104d0eb681e98cae8e66f
Monojit Saha <monojitsaha@Monojits-MacBook-Pro.local>
2025-03-30 12:07:55 +0530

    code complete

---
+++ nextjs-practice-project-2/pages/api/contact.js
@@ -0,0 +1,55 @@
+import { validateEmail } from "@/utils/sharedFunctions";
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !validateEmail(email) ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(400).json({ message: "Invalid input." });
+      return;
+    }
+
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    let client;
+
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.tl9k3.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority&appName=${process.env.mongodb_clustername}`;
+
+    try {
+      client = await MongoClient.connect(connectionString);
+    } catch (error) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: "Storing message failed!" });
+      return;
+    }
+
+    client.close();
+
+    res
+      .status(201)
+      .json({ message: "Successfully stored message!", data: newMessage });
+  }
+}
+
+export default handler;
