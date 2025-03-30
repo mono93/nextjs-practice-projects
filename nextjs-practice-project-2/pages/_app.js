1d04509ea8172f6fd6940ef3916fd18fb86b1b33
Monojit Saha <monojitsaha@Monojits-MacBook-Pro.local>
2025-03-30 12:11:49 +0530

    add meta info

--- nextjs-practice-project-2/pages/_app.js
+++ nextjs-practice-project-2/pages/_app.js
@@ -4,6 +4,9 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />;
     </Layout>
   );
